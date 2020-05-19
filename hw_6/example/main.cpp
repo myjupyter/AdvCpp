217f40580ef42fc9739021903594549f7934df38
Kirill Tkachuk <kirilltchkap@gmail.com>
2020-05-19 23:08:40 +0300

    Added configurator

--- hw_6/example/main.cpp
+++ hw_6/example/main.cpp
@@ -6,6 +6,7 @@
 #include "http_server.h"
 #include "http_packet.h"
 #include "global_log_func.h"
+#include "configurator.h"
 
 using namespace Network;
 using namespace Network::Services;
@@ -14,6 +15,12 @@ using namespace std::chrono_literals;
 int main() {
     Log::Logger& log = Log::Logger::getInstance();
     log.setGlobalLogger(Log::create_stderr_logger());
+
+    Config::Configurator& config = Config::Configurator::getInstance();
+    config.readConfigsFrom("./config/config.ini");
+
+
+    std::cout << Config::Configurator::get<int>("MyHttp", "events") << std::endl;
 
     try {
         HttpServer server({"127.0.0.1", 8080});
--- hw_6/example/src/main.cpp
+++ hw_6/example/src/main.cpp
@@ -1,16 +1,23 @@
 #include "myhttp.h"
+#include "configurator.h"
 
+using namespace Network;
 
 int main() {
+
     Logger& log = Logger::getInstance();
     log.setGlobalLogger(create_stderr_logger());
+
+    Config::Configurator& config = Config::Configurator::getInstance();
+    config.readConfigsFrom("./config/config.ini");
 
     set_level(Level::Info);
 
     try {
         MyHttp server({"0.0.0.0", 8081});
 
-        server.work(4, 2.5);
+        server.work(Config::Configurator::get<int>("MyHttp", "threads"),
+                    Config::Configurator::get<double>("MyHttp", "timeout"));
 
     } catch (std::runtime_error& err) {
         std::cout << err.what() << std::endl;
