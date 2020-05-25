19b3afb201003d7a48862db14f1b24fc000c1fac
Kirill Tkachuk <kirilltchkap@gmail.com>
2020-05-25 17:41:00 +0300

    template of working

--- hw_7/example/resource.cpp
+++ hw_7/example/resource.cpp
@@ -1,13 +1,62 @@
 #include <iostream>
+#include <utility>
+#include <algorithm>
+#include <ctime>
+
 #include "file.h"
 
+using Key = std::pair<uint64_t, uint64_t>;
+
+struct Data {
+    uint8_t payload[20] = {};
+};
+
+
+constexpr int SIZE = 10;
+
 int main() {
-
-    //ResourceManager manager = ResourceManager::getInstance();
-    //std::cout << manager.getResource("example/main.cpp") << std::endl;
     
-    MappedFile file(File("example/main.cpp"), MappedFile::in, MappedFile::shared);
+    // write indexies
+    {
+        BinaryFile file("./index", File::out);
 
+        for (int i = 0; i < SIZE; i++) {
+            std::string s = std::to_string(i);
+
+            Key ko = std::make_pair(std::hash<std::string>{}(s), sizeof(Data) * i);
+            file.write(&ko, sizeof(Key));
+        }
+    }
+
+    // write data
+    {
+        BinaryFile file("./data", File::out);
+
+        for (int i = 0; i < SIZE; i++) {
+            std::string s = std::to_string(i);
+
+            Data data;
+            std::strncpy(reinterpret_cast<char*>(data.payload), s.data(), s.size() > 20 ? 20 : s.size());
+
+            file.write(&data, sizeof(Data));
+        }
+    }
+
+
+    IndexFile<uint64_t, uint64_t> file(File{"./index"}, MappedFile::in, MappedFile::shared);
+    BinaryFile data_file("./data");
+
+
+    std::srand(std::time(nullptr));
+    for (int i = 0 ; i < 10; i++) {
+        int num = std::rand() % SIZE;
+        uint64_t key = std::hash<std::string>{}(std::to_string(num));
+        
+        Data data;
+        data_file.readAt(file.table_[key], &data, sizeof(Data));
+
+        std::cout << num << " " << data.payload << std::endl;
+    }
 
     return 0;
 }
--- hw_7/include/system/file.h
+++ hw_7/include/system/file.h
@@ -12,18 +12,21 @@
 #include <string>
 #include <stdexcept>
 #include <functional>
+#include <unordered_map>
+
+#include <iostream>
 
 class MappedFile;
 
 class File {
     public:
-        enum flags {
+        enum Flags {
             in  = O_RDONLY,
-            out = O_WRONLY,
+            out = O_WRONLY | O_CREAT | O_TRUNC,
         };
 
     public:
-        explicit File(const std::string& path, flags flag = File::in) {
+        explicit File(const std::string& path, Flags flag = Flags::in) {
             fd_ = ::open(path.c_str(), static_cast<int>(flag));
             if (fd_ == -1 || fstat(fd_, &info_) == -1) {
                 throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
@@ -49,17 +52,45 @@ class File {
             return *this;
         }
 
+        std::size_t getSize() const {
+            return info_.st_size;
+        }
 
-        ~File() {
-            if (fd_ != -1) {
+        virtual void close() {
+             if (fd_ != -1) {
                 ::close(fd_);
                 
                 fd_ = -1;
-                ::memset(&info_, '\0', sizeof(struct stat));
-            }
+                ::memset(&info_, '\0', sizeof(struct stat)); 
+             }
         }
 
-    private: 
+        virtual ~File() {
+            close();
+        }
+
+        virtual std::size_t write(const void* data, std::size_t size) {
+            ssize_t bytes = ::write(fd_, data, size);
+            if (bytes == -1) {
+                throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
+                                        "File::write");
+            }
+            return bytes;
+        }
+
+        virtual std::size_t read(void* data, std::size_t size) {
+            ssize_t bytes = ::read(fd_, data, size);
+            if (bytes == -1) {
+                throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
+                                        "File::write");
+            }
+            if (bytes == 0) {
+                close();
+            }
+            return bytes;
+        }
+
+    protected: 
         File() : fd_{-1} {}
         
         int fd_;
@@ -68,10 +99,77 @@ class File {
         friend MappedFile;
 };
 
+class BinaryFile : public File {
+    public:
+        using FILEPtr = std::unique_ptr<FILE>;
+    
+    public:
+        explicit BinaryFile(const std::string& path, Flags flag = Flags::in)
+            : File(path, flag) {
+            const char* mode = (flag == Flags::in) ? "rb" : "wb";
+            
+            FILE* ptr = fdopen(fd_, mode);
+            if (ptr == NULL) {
+                throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
+                                        "BinaryFile::BinaryFile");
+            }
+            file_ptr_ = FILEPtr(ptr);
+        }
+
+        std::size_t read(void* data, std::size_t size) override {
+            std::size_t bytes = ::fread(data, size, 1, file_ptr_.get());
+            if (bytes < 1) {
+                throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
+                                        "BinaryFile::read");
+            }
+            return bytes;
+        }
+
+        virtual void close() override {
+            if (file_ptr_ != nullptr) {
+                fclose(file_ptr_.release());
+            }
+        }
+
+        ~BinaryFile() {
+            close();
+        } 
+
+        std::size_t writeAt(long offset, const void* buf, std::size_t size) {
+            if (fseek(file_ptr_.get(), offset, SEEK_SET) < 0) {
+                throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
+                                        "BinaryFile::readAt");
+            }
+        
+            std::size_t bytes = write(buf, size);
+            rewind(file_ptr_.get());
+
+            return bytes;
+        }
+
+        std::size_t readAt(long offset, void* buf, std::size_t size) {
+            if (fseek(file_ptr_.get(), offset, SEEK_SET) < 0) {
+                throw std::system_error(std::make_error_code(static_cast<std::errc>(errno)),
+                                        "BinaryFile::readAt");
+            }
+
+            std::cout << offset << " " << ftell(file_ptr_.get()) << std::endl; 
+
+            std::size_t bytes = read(buf, size);
+            rewind(file_ptr_.get());
+            
+            return bytes;
+        }
+
+    private:
+        FILEPtr file_ptr_;
+};
+
 class MappedFile {
     public:
         using MappedFilePtr = std::unique_ptr<char, std::function<void(char*)>>;
-        
+        using FilePtr       = std::unique_ptr<File>;
+
         enum Mode {
             in = PROT_READ,
             out = PROT_WRITE
@@ -82,12 +180,11 @@ class MappedFile {
             unique = MAP_PRIVATE
         };
     
-    public:
-        
-        MappedFile(File&& file, Mode m, MapMode mm)
+    public:        
+        MappedFile(File&& file, Mode m = Mode::in, MapMode mm = MapMode::shared)
             : file_(std::move(file)) {
 
-            std::size_t size = file_.info_.st_size;
+            std::size_t size = file_.getSize();
             void* ptr = ::mmap(0, size, static_cast<int>(m), 
                                static_cast<int>(mm), file_.fd_, 0);
             if (ptr == MAP_FAILED) {
@@ -96,19 +193,47 @@ class MappedFile {
             }
 
             mapped_file_ = std::move(MappedFilePtr{reinterpret_cast<char*>(ptr), [size] (char* ptr) {
-                ::munmap(ptr, size);    
+                if (ptr != nullptr) {
+                    ::munmap(ptr, size);
+                }    
             }});
         }
 
-        ~MappedFile() {
-            
+        void close() {
+            mapped_file_.reset(nullptr);
+            file_.close();
         }
 
-    private:
+        ~MappedFile() = default;
+
+    protected:
         MappedFile() = delete;
 
         File file_;
         MappedFilePtr mapped_file_ = nullptr;
+};
+
+
+template <typename Key, typename T>
+class IndexFile : public MappedFile {
+    public:
+        using value_type = std::pair<Key, T>;
+
+    public:
+        IndexFile(File&& file, Mode m = Mode::in, MapMode mm = MapMode::shared)
+            : MappedFile(std::move(file), m, mm) {
+            void* ptr = mapped_file_.get();
+            std::size_t size = file_.getSize();
+            
+            for (uint64_t i = 0; i < size; i += sizeof(value_type)) {
+                table_.emplace(*(reinterpret_cast<value_type*>(reinterpret_cast<char*>(ptr) + i)));        
+            }
+
+            close();
+        }
+
+    public:
+        std::unordered_map<Key, T> table_;
 };
 
 #endif  // SYSTEM_FILE_H_
