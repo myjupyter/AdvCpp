ea934376edf3652c431923df00cafb0d25cf4d82
Kirill Tkachuk <kirilltchkap@gmail.com>
2020-05-29 17:50:36 +0300

    Completed

--- hw_7/example/include/myhttp.h
+++ hw_7/example/include/myhttp.h
@@ -33,7 +33,12 @@ static std::string access_log(const HttpPacket& request, const HttpPacket& respo
     return std::string(log_message.str());
 }
 
+
 class MyHttp : public HttpServer {
+    public:
+        using MethodHandler = std::function<HttpPacket(const HttpPacket& packet)>;
+        using HandlerMap    = std::unordered_map<std::string_view, MethodHandler>;
+
     public:
         explicit MyHttp(const Network::IpAddress& address)
             : HttpServer(address, nullptr) {}
@@ -43,35 +48,23 @@ class MyHttp : public HttpServer {
 
             HttpPacket response;
 
-            if (method == Method::GET) {
-                std::string req = request.toString();
-
-                ResourceManager& res_man = ResourceManager::getInstance();
-                std::string body = res_man.getResource(uri, true);
-
-                if (body.empty()) {
-                    response.makeResponse("1.1", Code::NOT_FOUND);
-                    response.setBody("404 Not Found");
-                } else {
-                    response.makeResponse("1.1", Code::OK);
-                    response.setBody(std::move(body));
-                }
-                
-            } else if (method == Method::PUT) {
-                response.makeResponse("1.1", Code::OK);
-                std::string buffer = request.getBody();
-                response.setBody(buffer);
-            } else if (method == Method::POST) {
+            if (handler_map_.contains(method)) {
+                response = handler_map_[method](request);
+            } else {
                 response.makeResponse("1.1", Code::NOT_FOUND);
-                response.setBody("This was POST");        
-            } else if (method == Method::DELETE) {
-                response.makeResponse("1.1", Code::NOT_FOUND);
-                response.setBody("This was DELETE");
+                response.setBody("404 Not Found"); 
             }
 
             info(access_log(request, response));
             return response;
         }
+
+        void HttpHandler(std::string_view method, MethodHandler handler) {
+            handler_map_[method] = handler;
+        }
+
+    private:
+        HandlerMap handler_map_;        
 };
 
 #endif  // EXAMPLE_MYHTTP_H_
--- hw_7/example/src/main.cpp
+++ hw_7/example/src/main.cpp
@@ -1,5 +1,7 @@
 #include "myhttp.h"
 #include "configurator.h"
+#include "file.h"
+
 
 using namespace Network;
 
@@ -13,9 +15,36 @@ int main() {
 
     set_level(Level::Info);
 
+
+    DataStorage storage(Config::Configurator::get<std::string>("Storage", "data"));
+    if (!storage.isSorted()) {
+        storage.sort();
+    }
+    storage.createIndex(Config::Configurator::get<std::string>("Storage", "index"));
+
     try {
         MyHttp server({Config::Configurator::get<std::string>("MyHttp", "ip"),
                        Config::Configurator::get<uint16_t>("MyHttp", "port")});
+
+
+        server.HttpHandler("GET", [&storage](const HttpPacket& req) -> HttpPacket {
+            auto [method, uri] = req.getRequestLine();
+            
+            HttpPacket response;            
+        
+            uri.erase(std::remove(uri.begin(), uri.end(), '/'), uri.end());
+
+            auto val = storage.search(std::hash<std::string>{}(uri));
+            if (val.has_value()) {
+                response.makeResponse("1.1", Code::OK);
+                std::string str(reinterpret_cast<char*>(val.value().payload));
+                response.setBody(str);
+            } else {
+                response.makeResponse("1.1", Code::NOT_FOUND);
+                response.setBody("404 Not Found");
+            }
+            return response;         
+        });
 
         server.work(Config::Configurator::get<int>("MyHttp", "threads"),
                     Config::Configurator::get<double>("MyHttp", "timeout"));
